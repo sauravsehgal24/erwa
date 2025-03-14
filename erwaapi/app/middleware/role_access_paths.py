054ac64eced440ea118c7a7d71cccbc44603b6ae
saurav sehgal <sauravsehgal44@gmail.com>
2025-03-13 22:31:51 -0400

    some changes

--- erwaapi/app/middleware/role_access_paths.py
+++ erwaapi/app/middleware/role_access_paths.py
@@ -1,4 +1,4 @@
 ROLE_PATHS = {
-        "ADMIN": ["/v1/hello","/v1/admin/get_admin_list"],  # Paths only admins can access
+        "ADMIN": ["/v1/hello","/v1/admin/get_admin_list","/v1/user/get_user_by_email"],  # Paths only admins can access
         "USER": ["/v1/hello","/v1/user/users", "/v1/user/settings","/v1/user/profile","/v1/user/get_user_by_email"],   # Paths only users can access
     }
