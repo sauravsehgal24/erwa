001170a0e546dd441b115aa56839a1de5b033140
Siddhi Patel <spatel538@myseneca.ca>
2025-03-13 22:22:04 -0400

    Add OCR api files for new endpoint

--- erwaapi/app/urls.py
+++ erwaapi/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import admin_view, user_view, hello_view
+from .views import admin_view, user_view, hello_view, ocr_view
 
 urlpatterns = [
     path('user/', include((user_view.user_patterns, "app"), namespace="user")),
     path('admin/', include((admin_view.user_patterns, "app"), namespace="admin")),
+    path('ocr/', include((ocr_view.user_patterns, "app"), namespace="ocr")),
     path('hello', hello_view.hello),
 ]
